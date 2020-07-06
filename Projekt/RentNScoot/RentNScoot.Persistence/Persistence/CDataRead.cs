69d1cb0b4b301f8928de3bfd6d8dd4e35f42ad65
Id652250 <61251684+fj-gruenewald@users.noreply.github.com>
2020-07-06 17:08:24 +0200

    Iteration2.Peristence
    added Persistence Architecture

---
+++ Projekt/RentNScoot/RentNScoot.Persistence/Persistence/CDataRead.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentNScoot.Persistence
+{
+    internal class CDataRead : IDataRead
+    {
+        //
+        internal CDataRead()
+        {
+        }
+
+        //
+        public int CountScooters()
+        {
+            return 666;
+        }
+    }
+}
---
+++ Projekt/RentNScoot/RentNScoot.Persistence/Persistence/CDataWrite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentNScoot.Persistence
+{
+    internal class CDataWrite : IDataWrite
+    {
+        //
+        internal CDataWrite()
+        {
+
+        }
+    }
+}
