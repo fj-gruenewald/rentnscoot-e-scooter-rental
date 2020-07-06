3fd4fe05d116145bc1115467c83d6b250c6aef24
Id652250 <61251684+fj-gruenewald@users.noreply.github.com>
2020-07-06 17:16:19 +0200

    Iteration2.Presentation
    added Presentation Architecture

---
+++ Projekt/RentNScoot/RentNScoot.Presentation/Presentation/ViewModels/CvmMain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentNScoot.Presentation.ViewModels
+{
+    public class CvmMain
+    {
+        //
+        private IAppCommands _appCommands;
+        private IAppQueries _appQueries;
+
+        //
+        public string NCars { get; }
+
+        //
+        public CvmMain(IAppCommands appCommands, IAppQueries appQueries)
+        {
+            _appCommands = appCommands;
+            _appQueries = appQueries;
+            NCars = $"Insgesamt {_appQueries.CountScooters()} Roller";
+        }
+    }
+}
