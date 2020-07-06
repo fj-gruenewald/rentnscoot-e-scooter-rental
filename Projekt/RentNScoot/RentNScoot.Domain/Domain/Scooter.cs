702b5c64328bc9709dd015930b8876629ec302a1
Id652250 <61251684+fj-gruenewald@users.noreply.github.com>
2020-07-06 16:56:07 +0200

    Iteration2.Domain
    Added the Domain Architecture

---
+++ Projekt/RentNScoot/RentNScoot.Domain/Domain/Scooter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentNScoot.Domain
+{
+    public class Scooter : IEntity
+    {
+        //Was für Attribute hat ein Roller
+        public string Id { get; private set; }
+        public string Make { get; }
+        public string Model { get; }
+
+
+        //Konstruktor zum erstellen neuer Einträge
+        public Scooter(string make, string model)
+        {
+            Id = System.Guid.NewGuid().ToString();
+            Make = make;
+            Model = model;
+        }
+
+        //Konstruktor zum lesen aus Datenbank (ID bekannt)
+        public Scooter(string id, string make, string model)
+        {
+            Id = id;
+            Make = make;
+            Model = model;
+        }
+    }
+}
