41376da6eda41f391d868049c330e82591b2ef82
Id652250 <61251684+fj-gruenewald@users.noreply.github.com>
2020-07-06 17:21:12 +0200

    Iteration2.Start
    Added the Start Architecture

--- Projekt/RentNScoot/RentNScoot.Start/App.xaml.cs
+++ Projekt/RentNScoot/RentNScoot.Start/App.xaml.cs
@@ -6,12 +6,48 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
+//
+using RentNScoot.Persistence.Factories;
+using RentNScoot.Application.Factories;
+using RentNScoot.Presentation.Factories;
+
 namespace RentNScoot.Start
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
+        //Persistence
+        private IDataRead _dataRead;
+        private IDataWrite _dataWrite;
+
+        //Application
+        private IAppQueries _appQueries;
+        private IAppCommands _appCommands;
+
+        //Presentation
+        private IDialog _dialog;
+
+
+        //EventHandler for StartUp
+        void AppStartUp(object sender, StartupEventArgs e)
+        {
+            //Dependency Injection Setup
+            //Dependency Inversion Principle 
+
+            //Persistence
+            _dataRead = AFactoryData.CreateReadInstance();
+            _dataWrite = AFactoryData.CreateWriteInstance();
+
+            //Application
+            _appQueries = AFactoryApp.CreateQueryInstance(_dataRead);
+            _appCommands = AFactoryApp.CreateCommandInstance(_dataWrite);
+
+            //Presentation
+            _dialog = AFactoryDialog.CreateSingleton(_appCommands, _appQueries);
+            _dialog.Show();
+
+        }
     }
 }
